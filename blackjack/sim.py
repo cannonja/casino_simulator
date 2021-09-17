5adeb81618c823901c19b97d73b47b58e9096d94
Jack Cannon <jack.cannon@intel.com>
2021-09-17 02:32:46 -0700

    Added logic for evaluating hands

--- blackjack/sim.py
+++ blackjack/sim.py
@@ -1,5 +1,6 @@
 from itertools import product
 import random
+from players import BJDealer, BJPlayer
 
 ### Build shoe and shuffle
 NUM_DECKS = 2
@@ -12,23 +13,38 @@ for _ in range(4):
 
 ### Deal initial hand
 NUM_PLAYERS = 3
-dealer = []
-players = {f'p{i}': [] for i in range(1, NUM_PLAYERS + 1)}
+dealer = BJDealer()
+players = {f'p{i}': BJPlayer(100) for i in range(1, NUM_PLAYERS + 1)}
 
 for _ in range(2):
     for p in players:
-        players[p].append(shoe.pop()[0])
-    dealer.append(shoe.pop()[0])
+        ret = players[p].update_bank_roll(-10)
+        if ret == True:
+            shoe = players[p].hit(shoe)
+    shoe = dealer.hit(shoe)
 
 ### Play
 # Dealer has black jack
+if dealer.hand_value == 21:
+    for p in players:
+        if players[p].hand_value == 21:
+            players[p].update_bank_roll(10)
+else:
+    # Check for players with blackjack
+    for p in players:
+        if players[p].hand_value == 21:
+            players[p].update_bank_roll(10)
 
-# Players have blackjack
+
+
+            
+
+# Players have black jack
 
 # Player moves
-#basic_strategy(player_hand, dealer_card, shoe)
-#no_bust_strategy(player_hand, dealer_card, shoe)
+#basic_strategy(player, dealer_card, shoe)
+#no_bust_strategy(player, dealer_card, shoe)
 
 # Dealer moves
-#dealer_strategy(dealer_hand, shoe)
+#dealer_strategy(dealer, shoe)
 
