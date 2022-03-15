05bbc82cbd066281f22d6e25c4000f7203b84eea
Jack Cannon <jack.cannon@intel.com>
2022-03-15 01:39:14 -0700

    Finished simulation code for simple case

--- blackjack/players.py
+++ blackjack/players.py
@@ -2,13 +2,88 @@ from cards import Hand
 
 
 class BJPlayer:
-    def __init__(self, bank_roll=None, strategy=None):
+    def __init__(self, bet=10, bank_roll=None, strategy=None):
         self.hands = None
         self.hand = None
+        self.hand_committed = None
+        self.active = None
         self.reset_hand()
         self.strategy = 'dealer' if strategy is None else strategy
+        self._bet = bet
+        self.bet = bet
         self.bank_roll = bank_roll
-        self.active = True
+        self.history = None
+        self.history_committed = None
+        self.reset_history()
+    
+
+    def reset_bet(self, bet=None):
+        if bet is None:
+            self.bet = self._bet
+        else:
+            self.bet = bet
+
+    
+    def place_bet(self, bet=None):
+        if bet is None:
+            bet = self.bet
+        else:
+            self.bet = bet
+        self.update_bank_roll(-bet)
+
+
+    def win_bet(self, bet=None):
+        if bet is None:
+            bet = self.bet
+        self.update_bank_roll(bet)
+
+    
+    def play(self, shoe, dealer_card):
+        if self.strategy == 'no_bust':
+            self._no_bust(shoe)
+        elif self.strategy == 'dealer':
+            self._dealer(shoe)
+
+
+    def _no_bust(self, shoe):
+        while self.active:
+            if self.hand.hand_value < 12:
+                self.hit(shoe)
+                if self.hand.busted:
+                    self.deactivate()
+            else:
+                # Stay
+                self.deactivate()
+
+
+    def _dealer(self, shoe):
+        while self.active:
+            if self.hand.hand_value < 17:
+                self.hit(shoe)
+                if self.hand.busted:
+                    self.deactivate()
+            else:
+                self.deactivate()
+
+    
+    def commit_win(self):
+        self._commit('win')
+
+    
+    def commit_loss(self):
+        self._commit('loss')
+
+
+    def commit_push(self):
+        self._commit('push')
+
+
+    def _commit(self, result):
+        self.history['hands'].append(self.hands)
+        self.history['bets'].append(self.bet)
+        self.history['bank_roll'].append(self.bank_roll)
+        self.history['results'].append(result)
+        self.hand_committed = True
 
     
     def hit(self, shoe):
@@ -31,6 +106,17 @@ class BJPlayer:
     def reset_hand(self):
         self.hands = [Hand()]
         self.hand = self.hands[0]
+        self.hand_committed = False
+        self.activate()
+
+
+    def reset_history(self):
+        self.history = {
+            'hands': [],
+            'bets': [],
+            'bank_roll': [],
+            'results': []
+            }
 
     
     def hand_summary(self):
@@ -44,5 +130,10 @@ class BJDealer(BJPlayer):
     def __init__(self):
         super().__init__()
 
+    
     def get_showing_card(self):
         return self.hand.cards[1][0]
+
+
+    def play(self, shoe):
+        self._dealer(shoe)
--- blackjack/sim.py
+++ blackjack/sim.py
@@ -2,80 +2,178 @@ from itertools import product
 import random
 from players import BJDealer, BJPlayer
 from cards import Hand, Deck
+import pandas as pd
 
-### Build shoe and shuffle
-NUM_DECKS = 2
-shoe = Deck() * NUM_DECKS
-for _ in range(4):
-    shoe.shuffle()
 
-### Play hand
-BET = 10
-NUM_PLAYERS = 3
-dealer = BJDealer()
-players = {f'p{i}': BJPlayer(100) for i in range(1, NUM_PLAYERS + 1)}
 
-# Place bets
-for p in players:
-    players[p].update_bank_roll(-BET)
 
-# Deal first two cards
-for _ in range(2):
+def play_round(dealer, players, shoe):
+    if len(shoe) < 20:
+        return "Round aborted, reached end of shoe"
+
+    # Place bets
+    dealer.activate()
     for p in players:
-        players[p].hit(shoe)
-    dealer.hit(shoe)
+        players[p].activate()
+        players[p].place_bet()
 
-# First check for dealer blackjack and continue hand if false (removing players as needed)
-if dealer.hand.hand_value == 21:
-    # Assume push if player also has blackjack
-    # Deactivate all players since hand is over
-    for p in players:
-        players[p].deactivate()
-        if players[p].hand.hand_value == 21:
-            players[p].update_bank_roll(BET)
-else:
-    # Payout players with blackjack
-    # Deactivate these players
-    for p in players:
-        if players[p].hand.hand_value == 21:
+    # Deal first two cards
+    for _ in range(2):
+        for p in players:
+            players[p].hit(shoe)
+        dealer.hit(shoe)
+
+    # First check for dealer blackjack and continue hand if false (removing players as needed)
+    if dealer.hand.hand_value == 21:
+        # Assume push if player also has blackjack
+        # Deactivate all players since hand is over
+        for p in players:
             players[p].deactivate()
-            players[p].update_bank_roll(BET * 3)
+            if players[p].hand.hand_value == 21:
+                players[p].win_bet()
+                players[p].commit_push()
+    else:
+        # Payout players with blackjack
+        # Deactivate these players
+        for p in players:
+            if players[p].hand.hand_value == 21:
+                players[p].deactivate()
+                players[p].win_bet(players[p].bet * 2.5)
+                players[p].commit_win()
 
-if sum([p.active for _, p in players.items()]) > 0:
-    # Player moves - no bust
-    player_bet = BET
-    dealer_card = dealer.get_showing_card()
-    for p in players:
+    # Play with remaining active players
+    if sum([p.active for _, p in players.items()]) > 0:
+        # Player moves - no bust
+        player_bet = BET
+        dealer_card = dealer.get_showing_card()
+        for p in players:
+            players[p].play(shoe, dealer_card)
+            '''
+            if players[p].active:
+                staying = False
+                while not staying and not players[p].hand.busted:
+                    if players[p].hand.hand_value < 12:
+                        players[p].hit(shoe)
+                    else:
+                        staying = True
+            '''
+
+        # Dealer moves - stand on soft 17
+        dealer.play(shoe)
+        '''
         staying = False
-        while not staying and not players[p].hand.busted:
-            if players[p].hand.hand_value < 12:
-                players[p].hit(shoe)
-            else:
+        while not staying and not dealer.hand.busted:
+            if dealer.hand.hand_value > 16:
                 staying = True
+            else:
+                dealer.hit(shoe)
+        '''
 
-    # Dealer moves - stand on soft 17
-    staying = False
-    while not staying and not dealer.hand.busted:
-        if dealer.hand.hand_value > 16:
-            staying = True
-        else:
-            dealer.hit(shoe)
+    # Score 
+    for p in players:
+        if not players[p].hand_committed:
+            if players[p].hand.busted:
+                # Busted
+                players[p].commit_loss()
+            elif players[p].hand.hand_value > dealer.hand.hand_value or dealer.hand.busted:
+                # Won
+                players[p].win_bet(players[p].bet * 2)
+                players[p].commit_win()
+            elif players[p].hand.hand_value == dealer.hand.hand_value:
+                # Push
+                players[p].win_bet()
+                players[p].commit_push()
+            else:
+                # Not busted, but didn't beat the dealer
+                players[p].commit_loss()
 
-# Score
-for p in players:
-    if not players[p].hand.busted:
-        if players[p].hand.hand_value > dealer.hand.hand_value or dealer.hand.busted:
-            players[p].update_bank_roll(BET * 2)
-        elif players[p].hand.hand_value == dealer.hand.hand_value:
-            players[p].update_bank_roll(BET)
 
-print(dealer.hand)
-print()
-
-for _, p in players.items():
-    print(p.hand)
-    print(f"Bank Roll: {p.bank_roll}")
+    '''
+    # Print Results
+    print(dealer.hand)
     print()
+
+    for _, p in players.items():
+        print(p.hand)
+        print(f"Bank Roll: {p.bank_roll}")
+        print()
+    '''
+
+
+
+def build_shoe(num_decks):
+    shoe = Deck() * num_decks
+    for _ in range(4):
+        shoe.shuffle()
+
+    return shoe
+
+
+
+
+if __name__ == '__main__':
+    ### Build shoe and shuffle
+    NUM_DECKS = 6
+    shoe = build_shoe(NUM_DECKS)
+
+    ### Initialize players
+    BET = 10
+    NUM_PLAYERS = 1
+    dealer = BJDealer()
+    players = {f'p{i + 1}': BJPlayer(bet=BET, bank_roll=300, strategy='no_bust') for i in range(NUM_PLAYERS)}
+
+    ### Run simulation
+    NUM_HANDS = 100000
+    for _ in range(NUM_HANDS):
+        if len(shoe) < 20:
+            shoe = build_shoe(NUM_DECKS)
+        play_round(dealer, players, shoe)
+
+        # Reset hands
+        dealer.reset_hand()
+        for p in players:
+            players[p].reset_hand()
+
+        # Update bets if needed (Martingale)
+        for p in players:
+            last_result = players[p].history['results'][-1]
+            last_bet = players[p].bet
+            if last_result == 'loss':
+                players[p].reset_bet(last_bet * 2)
+            elif last_result == 'win':
+                players[p].reset_bet()
+
+    '''
+    ### View history
+    for p in players:
+        print(players[p].history['results'])
+        print(players[p].history['bank_roll'])
+        print()
+    '''
+    results = players['p1'].history['results']
+    i = -1
+    last = None
+    streaks = []
+    for r in results:
+        if r != last:
+            i += 1
+        last = r
+        streaks.append(i)
+    sdf = pd.DataFrame({'streak': streaks, 'result': results})
+    loss_dist = sdf.groupby(['streak', 'result'], as_index=False) \
+                   .size() \
+                   .query("result == 'loss'") \
+                   .groupby(['result', 'size']) \
+                   .size() \
+                   .transform(lambda x: x / x.sum())
+    print(loss_dist.cumsum())
+
+
+
+    
+
+
+
 
 
 
